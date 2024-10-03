199149a9f098512d839d370d7ec06019dfc22b7d
shakhbazow <emil@csu.fullerton.edu>
2024-10-02 23:47:25 -0500

    new
    last made changes

--- AVLTree.java
+++ AVLTree.java
@@ -1,6 +1,6 @@
 
 /*
- * *** YOUR NAME GOES HERE / YOUR SECTION NUMBER ***
+ * *** Emil Shahbazov / 272-01 ***
  *
  * This java file is a Java object implementing simple AVL Tree.
  * You are to complete the deleteElement method.
@@ -361,8 +361,77 @@ class LUC_AVLTree {
          * code for each. You can also look at the method InsertElement, as it has do
          * do many of the same things as this method.
          */
+        if (node == null) {
+            return null;
+        }
+
+        // perform bst delete
+        if (value < node.value) {
+
+            node.leftChild = deleteElement(value, node.leftChild); // value is in left subtree
+        } else if (value > node.value) {
+
+            node.rightChild = deleteElement(value, node.rightChild);  // value is in the right subtree
+        } else {
+
+            if (node.leftChild == null || node.rightChild == null) {
+                // has one child or no child
+                Node temp = (node.leftChild != null) ? node.leftChild : node.rightChild;
+
+                if (temp == null) {
+                    // no chuld case
+                    node = null;
+                } else {
+                    // one child case
+                    node = temp;
+                }
+            } else {
+               // node has two children, get inorder priority
+                Node temp = minValueNode(node.rightChild);
+
+                node.value = temp.value;
+                // delete
+                node.rightChild = deleteElement(temp.value, node.rightChild);
+            }
+        }
+
+        // if tree has one node then return
+        if (node == null) {
+            return null;
+        }
+
+        // update height of the current node
+        node.height = getMaxHeight(getHeight(node.leftChild), getHeight(node.rightChild)) + 1;
+
+        // get the balance factor to check whether this node became unbalanced
+        int bf = getBalanceFactor(node);
+
+        // if node unbalancedd perform rotations
+
+        // ll
+        if (bf > 1 && getBalanceFactor(node.leftChild) >= 0) {
+            return LLRotation(node);
+        }
+
+        //lr
+        if (bf > 1 && getBalanceFactor(node.leftChild) < 0) {
+            node.leftChild = RRRotation(node.leftChild);
+            return LLRotation(node);
+        }
+
+        // rr
+        if (bf < -1 && getBalanceFactor(node.rightChild) <= 0) {
+            return RRRotation(node);
+        }
+
+        // rl
+        if (bf < -1 && getBalanceFactor(node.rightChild) > 0) {
+            node.rightChild = LLRotation(node.rightChild);
+            return RRRotation(node);
+        }
 
         return node;
+       // return node;
     }
 
 
@@ -426,7 +495,7 @@ class LUC_AVLTree {
      *        / \                        /   \    /  \
      *       zl  zr                     zr   zl  yr  xr
      *
-     *  @param X - node that is out of balance
+     * //@param X - node that is out of balance
      *
      *  @return Y - new top node of [sub]tree after rotation
      */
@@ -463,7 +532,7 @@ class LUC_AVLTree {
      *           / \                 /   \    /  \
      *          zl  zr              yl   zl  zr  xr
      *
-     *  @param X - node that is out of balance
+     * // @param X - node that is out of balance
      *
      *  @return Z - new top of the [sub]tree after rotation
      */
@@ -503,7 +572,7 @@ class LUC_AVLTree {
      *                     / \                 /   \    /  \
      *                    zl  zr              xl   yl  zl  zr
      *
-     *  @param X - node that is out of balance
+     * //  @param X - node that is out of balance
      *
      *  @return Y - new top node of [sub]tree after rotation
      */
@@ -539,7 +608,7 @@ class LUC_AVLTree {
      *          / \                       /   \    /  \
      *         zl  zr                    xl   zl  zr  yr
      *
-     *  @param X - node that is out of balance
+     * // @param X - node that is out of balance
      *
      *  @return Z - new top of the [sub]tree after rotation
      */
--- TreeProblems.java
+++ TreeProblems.java
@@ -1,5 +1,5 @@
 /*
- * *** YOUR NAME GOES HERE / YOUR SECTION NUMBER ***
+ * *** Emil Shahbazov / 272-01 ***
  *
  * This java file contains several simple tree problems that need to be
  * codified. These routines  must use the TreeMap and TreeSet library
@@ -14,11 +14,11 @@ public class TreeProblems {
   /**
    * Method different()
    *
-   * Given two TreeSets of integers, return a TreeSet containing all elements 
+   * Given two TreeSets of integers, return a TreeSet containing all elements
    * that are NOT in both sets. In other words, return a TreeSet of all the
    * elements that are in one set but not the other.
    */
-  
+
   public static Set<Integer> different(Set<Integer> setA, Set<Integer> setB) {
 
     // INSERT CODE HERE - DO NOT FORGET TO PLACE YOUR NAME ABOVE
@@ -27,7 +27,16 @@ public class TreeProblems {
     // *several* lines of code. Hint: create two temporary TreeSets and utilize the
     // methods retainAll(), addAll(), and removeAll(). But in the end, get something to work.
 
-    return setA;
+    Set<Integer> result = new TreeSet<>(setA);
+    // create new treeset add all elements from setB to the result
+    result.addAll(setB);
+    // set for common elements
+    Set<Integer> common = new TreeSet<>(setA);
+    common.retainAll(setB);
+    // remove common elements from result
+    result.removeAll(common);
+    return result;
+
   }
 
 
@@ -35,14 +44,21 @@ public class TreeProblems {
    * Method removeEven()
    *
    * Given a treeMap with the key as an integer, and the value as a String,
-   * remove all <key, value> pairs where the key is even. 
+   * remove all <key, value> pairs where the key is even.
    */
 
   public static void removeEven(Map<Integer, String> treeMap) {
 
     // INSERT CODE HERE.
 
-    return;
+    Iterator<Integer> iterator = treeMap.keySet().iterator(); // iterator over key set
+    while (iterator.hasNext()) {
+      Integer key = iterator.next();
+      if (key % 2 == 0) {
+        iterator.remove();
+      }
+    }
+
   }
 
 
@@ -56,9 +72,9 @@ public class TreeProblems {
   public boolean treesEqual(Map<Integer, String> tree1,Map<Integer, String> tree2 ) {
 
     // INSERT CODE HERE
+    return tree1.equals(tree2);
 
-    return false;
 
   }
 
-} // end treeProblems class
+}
